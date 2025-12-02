c94d4d4367b649b697373c37dd553ce0f71e969a
amrata.deokar <amrata.deokar@gmail.com>
2025-12-02 06:54:32 +0530

    Updated code in input_function.py

--- input_function.py
+++ input_function.py
@@ -1,1 +1,7 @@
-print("Second project code") 
+input()
+name=input("what is your name?")
+print("Hello",name)
+input(123)#by default number as a string
+x=int(input("Enter a value:"))
+print("x =",x)
+print("Hello"+" "+input("What is your name?"))
