a1a0980984374341c0030eb4aa76363ec6302ccd
amrata.deokar <amrata.deokar@gmail.com>
2025-12-03 18:44:36 +0530

    Added type_casting.py

---
+++ type_casting.py
@@ -0,0 +1,32 @@
+print(len("Amrata Deokar"))
+
+print(len("12345"))
+
+#print(len(12345))
+
+length=len("Amrata")
+print("Your name has"+" "+str(length)+" "+"character")
+
+new_length=str(length)
+print(type(new_length))
+
+"""
+int
+float
+str
+"""
+
+print(int("10")+int("10"))
+
+print(10+float("10"))
+
+"""name="Jenny"
+print(10+int(name))"""#invalid
+
+name="123"
+print(10+int(name))
+
+first_number=input("Enter first number:")
+second_number=input("Enter second number:")
+sum=int(first_number)+int(second_number)
+print(sum)
