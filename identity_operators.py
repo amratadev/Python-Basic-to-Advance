22a4d1f02ca39eed394d95e4a2b44af63eb864f7
amrata.deokar <amrata.deokar@gmail.com>
2025-12-04 21:22:58 +0530

    Added identity_operators.py to Python-Basic-to-Advance repo

--- identity_operators.py
+++ identity_operators.py
@@ -1,8 +1,17 @@
-a=5
-b=2
-c='5'
+a = "Amrata"
+b = "amrata"
+c = '5'
+
 print(a is b)
+
 print(a is not c)
+
 print(id(a))
-print(id(b))
-print(id(c))
+
+List1=["Amrata","Hrutuja"]
+List2=["Amrata","Hrutuja"]
+print(List1 is not List2)
+print(id(List1))
+print(id(List2))
+#- Small integers (usually from -5 to 256) are pre‑allocated and reused.
+#- In Python, every time you create a new list with [...], Python allocates a new object in memory.
