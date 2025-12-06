80e67600c02f6fa2ca0de54744abdbdec04831d3
amrata.deokar <amrata.deokar@gmail.com>
2025-12-06 20:20:59 +0530

    Added Nested_else_if.py to Python-Basic-to-Advance repo

---
+++ Nested_else_if.py
@@ -0,0 +1,13 @@
+height=(int(input("What is your height in feet :")))
+if height>=3:
+   print("You can ride")
+   age=(int(input("What is your age:")))
+   if age<=18:
+     print("Pay 250 Rs")
+   else:
+     print("Pay 500 Rs")
+else:
+  print("Cant Ride")
+print("Bye")
+
+                  
