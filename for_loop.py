d44cb837f189dd536f2b09237adf1c34a1323513
amrata.deokar <amrata.deokar@gmail.com>
2025-12-13 16:51:47 +0530

    Added for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+name='Jenny'
+for i in name:
+    print(i)
+names=['Jenny','Ram','Shyam']
+for i in names:
+    print(i)
+    if i=='Jenny':
+      print("Hey,It's me")
+numbers=[2,3,5,-2,10]
+squares=[]
+for i in numbers:
+   square=i**2
+   squares.append(square)
+print("The list of squares is:",squares)
