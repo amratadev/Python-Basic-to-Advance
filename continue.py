1bdd69623e6b9843d9c7384c077d1585f8206f9b
amrata.deokar <amrata.deokar@gmail.com>
2025-12-17 21:07:48 +0530

    continue.py

---
+++ continue.py
@@ -0,0 +1,34 @@
+
+for count in range(1,11):
+    print(count)
+    count+=1
+    if count==7:
+        continue
+    print("Hi")
+print("Out from loop")
+
+
+
+count=1
+while count<=10:
+    print(count)
+    count+=1
+    if count==7:
+        continue
+    print("Hi")
+print("Out from loop")
+
+for count in range(1,11):
+    pass    
+        
+count=1
+while count<=10:
+    print(count)
+    count+=1
+    if count==7:
+        pass
+    print("Hi")
+print("Out from loop")   
+
+def fun1():
+  pass
