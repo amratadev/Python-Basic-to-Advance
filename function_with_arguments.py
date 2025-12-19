4f701b7f0a5c932e7e68eff4109b6250e3de4336
amrata.deokar <amrata.deokar@gmail.com>
2025-12-19 09:18:37 +0530

    function_with_arguments.py

---
+++ function_with_arguments.py
@@ -0,0 +1,10 @@
+def greet(name):
+    print(f"Hi,{name}")
+    print("Are you from cs Department?")
+greet("Amrata")
+
+def add(a,b):#parameter or formal parameter
+    c=a+b
+    print(f"sum is {c}")
+add(5,7)#Argument or actual parameter
+add(8,-9)
